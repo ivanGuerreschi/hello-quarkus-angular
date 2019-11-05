6d62106e8738eeee263b3eb4b9e92ec270c2b4df
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2019-11-05 16:24:12 +0100

    bug call servlet

--- src/main/java/it/ivanguerreschi/passwordsmanager/SampleServlet.java
+++ src/main/java/it/ivanguerreschi/passwordsmanager/SampleServlet.java
@@ -9,18 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/servlet/*"})
+@WebServlet(urlPatterns = { "/servlet/*" })
 public class SampleServlet extends HttpServlet {
-
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	@Override
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String path = req.getPathInfo();
-
         if (path.equals("/make-external-call")) {
             // Fake making an external call without involving the UI
             // e.g. OAuth Authentication Flow will have a few of these, resulting in
@@ -29,19 +27,18 @@ public class SampleServlet extends HttpServlet {
         } else if (path.equals("/callback")) {
             // Redirect back to a path controlled by the Angular client
             String redirectPath = "/clientCallback";
-            
+
             boolean proxy = Boolean.getBoolean("ui.proxy");
             if (proxy && checkProxyIsRunning()) {
                 redirectPath = "http://localhost:4200" + redirectPath;
             }
-            
             resp.sendRedirect(redirectPath);
         } else {
             resp.sendError(404);
         }
     }
-	
-	private boolean checkProxyIsRunning() {
+
+    private boolean checkProxyIsRunning() {
         try (Socket s = new Socket("localhost", 4200)) {
             return true;
         } catch (IOException e) {
--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,31 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {BehaviorSubject, Observable, Subject} from 'rxjs';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {take} from 'rxjs/operators';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-      <img src="/assets/quarkus_logo_horizontal_rgb_1280px_reverse.svg"/>
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
     <router-outlet></router-outlet>
   `,
   styles: []
@@ -37,22 +17,22 @@ export class AppComponent {
 @Component({
   selector: 'app-default',
   template: `
-    In <b>default</b> component. 
-    <a [routerLink]="['/other']">Other</a> | 
+    In <b>default</b> component.
+    <a [routerLink]="['/other']">Other</a> |
     <a [routerLink]="['/rest']">Rest</a> |
-    <a href="/servlet/make-external-call">External</a>
+    <a href="{{externalUrl}}">External</a>
   `,
   styles: []
 })
 export class DefaultComponent {
-	
-	externalUrl = '/servlet/make-external-call';
 
-	 constructor() {
-    	if (window.location.port === "4200") {
-      		this.externalUrl = "http://localhost:8080" + this.externalUrl;
-    	}
-	}
+  externalUrl = '/servlet/make-external-call';
+
+  constructor() {
+    if (window.location.port === '4200') {
+      this.externalUrl = 'http://localhost:8080' + this.externalUrl;
+    }
+  }
 }
 
 @Component({
@@ -75,22 +55,22 @@ export class OtherComponent {
   styles: []
 })
 export class RestComponent implements OnInit {
-  msg$: Subject<string> = new BehaviorSubject<string>("");
+  msg$: Subject<string> = new BehaviorSubject<string>('');
 
-  constructor(private _http: HttpClient) {
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
-    this._http.get('/api/hello', {responseType: 'text'})
-    .pipe(
-      take(1)
-    )
-    .subscribe(
-      msg => this.msg$.next(msg),
-      err => {
-        console.error(err);
-        this.msg$.next("An error happened - see the console log for details");
-      });
+    this.http.get('/api/hello', { responseType: 'text' })
+      .pipe(
+        take(1)
+      )
+      .subscribe(
+        msg => this.msg$.next(msg),
+        err => {
+          console.error(err);
+          this.msg$.next('An error happened - see the console log for details');
+        });
   }
 }
 
